0213c4ba1b8e01c60601588183728ff409a38af3
marchamil <74551298+marchamil@users.noreply.github.com>
2021-02-22 17:11:49 +0100

    New map with 3 layers
    Now we use Gamla stan. We have three layers, the satellite map with heights, the faked pollution and the road map

---
+++ Assets/Scripts/hide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour
+{
+
+    float size = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown("[1]") || Input.GetKeyDown("1"))
+         {
+             if (this.transform.localScale == new Vector3(0, 0, 0))
+                 this.transform.localScale = new Vector3(size, size, size);
+             else
+                 this.transform.localScale = new Vector3(0, 0, 0);
+         } 
+    }
+}
